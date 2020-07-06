9b6e103f3640154ebad4504aaa45f0fa1e63b6d2
FrederickWilliams <fredericktwilliams89@gmail.com>
2020-07-06 11:13:25 -0400

    weekend assignments

---
+++ 002_WordCount/pythonMR.py
@@ -0,0 +1,15 @@
+from mrjob.job import MRJob
+class mrWordCount(MRJob):
+
+    def mapper(self, key, line):
+        for word in line.split(' '):
+            yield word.lower(), 1
+
+    def combiner(self, word, occurrences):
+        yield word, sum(occurrences)
+
+    def reducer(self, word, occurrences):
+        yield word, sum(occurrences)
+
+if __name__ == '__main__':
+    mrWordCount.run()
