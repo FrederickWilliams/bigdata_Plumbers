201390f4dd8763804e50af23cfb09a5a253a8ec6
Frederick Williams <36410092+FrederickWilliams@users.noreply.github.com>
2020-07-13 06:11:20 -0400

    Add files via upload

---
+++ 003_hadoop_wordcount/mapper.py
@@ -0,0 +1,14 @@
+"""mapper.py"""
+
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # tab-delimited; the trivial word count is 1
+        print '%s\t%s' % (word, 1)
